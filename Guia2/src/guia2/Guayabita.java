f7893144251680e240850a8afb78b505c1caf322
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2015-11-15 01:12:25 -0500

    Punto 10

--- Guia2/src/guia2/Guayabita.java
+++ Guia2/src/guia2/Guayabita.java
@@ -1,5 +1,92 @@
 package guia2;
 
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Guayabita {
-    
+
+	public static void main(String[] args) throws IOException {
+		System.out.print("JUGAR GUAYABITA !!");
+		Scanner leer = new Scanner(System.in);
+		Random aleatorio = new Random();
+		
+		int acumulado = 10;
+		int cantidadJugadores = 0;
+		int tiro1 = 0;
+		int tiro2 = 0;
+		int cantidadApostar = 0;
+		boolean valorApostadoOk = false;
+		
+		//1. Pedir la cantidad de jugadores
+		System.out.print("\n\nCantidad jugadores: ");
+		cantidadJugadores = leer.nextInt();
+		String[] vcJugadores = new String[cantidadJugadores];
+		
+		//2. Pedir los nombres de los jugadores
+		System.out.print("\n");
+		for(int i=0 ; i<vcJugadores.length ; i++) {
+			System.out.print("Nombre del jugador " + (i + 1) + ": ");
+			vcJugadores[i] = leer.next();
+		}
+		
+		while (acumulado > 0) {
+			//2. Turnos de los jugadores
+			for(int i=0 ; i<vcJugadores.length ; i++) {
+				tiro1 = 0;
+				tiro2 = 0;
+				valorApostadoOk = false;
+				cantidadApostar = 0;
+				
+				System.out.print("\n\nTurno de " + vcJugadores[i]);
+				System.out.print("\nAcumulado: " + acumulado);
+				
+				tiro1 = aleatorio.nextInt(6) + 1;
+				System.out.print("\nTiro inicial: " + tiro1);
+				
+				if(tiro1 == 1 || tiro1 == 6) {
+					System.out.print("\nEl tiro inicial fue 1 o 6, se coloca una moneda en el acumulado");
+					acumulado++;
+				} else {					
+					System.out.print("\nCantidad a apostar: ");
+					
+					while (!valorApostadoOk) {
+						cantidadApostar = leer.nextInt();
+						if(cantidadApostar > acumulado) {
+							System.out.print("\nLa cantidad a apostar no puede ser mayor que el acumulado. Ingrese de nuevo la cantidad a apostar: ");
+						} else if(cantidadApostar <= 0) {
+							System.out.print("\nLa cantidad a apostar no puede ser menor o igual a 0. Ingrese de nuevo la cantidad a apostar: ");
+						}  
+						else {
+							valorApostadoOk = true;
+						}
+					}
+
+					tiro2 = aleatorio.nextInt(6) + 1;
+					System.out.print("\nSegundo tiro: " + tiro2);
+					
+					if(tiro2 > tiro1) {
+						System.out.print("\nSegundo tiro mayor a primer tiro, usted gana: " + cantidadApostar);
+						acumulado -= cantidadApostar;
+					} else {
+						System.out.print("\nSegundo tiro menor o igual primer tiro, usted pierde: " + cantidadApostar);
+						acumulado += cantidadApostar;
+					}
+					
+					if(acumulado == 0) {
+						break;
+					}
+				}
+			}
+			
+			if(acumulado == 0) {
+				System.out.print("\n\nEl acumulado llego a 0");
+				//System.out.print("\nGanador: " + vcJugadores[i]);
+				System.out.print("\nFin del juego");
+			}
+		}
+		
+		leer.close();
+	}
+
 }
---
+++ Guia2/src/guia2/punto10/Factura.java
@@ -0,0 +1,118 @@
+package guia2.punto10;
+
+import java.util.Scanner;
+
+public class Factura {
+	
+	static int FILAS_FACTURA = 11;
+	static int COLUMNAS_FACTURA = 7;
+	static int FILAS_PRODUCTO = 10;
+	
+	public static void main(String[] args) {		
+		
+		Object[][] matrizProductos = {
+				{1, "Leche", 2000.0, true},
+				{2, "Maiz", 1200.0, true},
+				{3, "Azucar", 1200.0, false},
+				{4, "Cerveza", 1300.0, false},
+				{5, "Lulo", 800.0, true},
+				{6, "Carne", 12000.0, true},
+				{7, "Ron", 12500.0, false},
+				{8, "Huevo", 200.0, false},
+				{9, "Pan", 300.0, true},
+				{10, "Lechuga", 1500.0, true}
+		};
+		
+		int idProducto = 0;
+		double cantidadProducto = 0;
+		int productosIngresados = 0;
+		Object[][] matrizFactura = new Object[FILAS_FACTURA][COLUMNAS_FACTURA];
+		
+		Scanner leer = new Scanner(System.in);
+				
+		for(int i=0 ; i<FILAS_FACTURA ; i++) {
+			System.out.print("Digite el ID del producto: ");
+			idProducto = leer.nextInt();
+			System.out.print("Digite el la cantidad del producto: ");
+			cantidadProducto = leer.nextDouble();
+			matrizFactura[i][0] = i;
+			matrizFactura[i][1] = idProducto;
+			matrizFactura[i][2] = obtenerNombreProducto(matrizProductos, idProducto);
+			matrizFactura[i][3] = cantidadProducto;
+			matrizFactura[i][4] = obtenerValorProducto(matrizProductos, idProducto);
+			matrizFactura[i][5] = calcularIvaValorProducto(matrizProductos, idProducto);
+			matrizFactura[i][6] = ((double)matrizFactura[i][3] * (double)matrizFactura[i][4]) + (double)matrizFactura[i][5];
+			productosIngresados++;
+			System.out.print("\nDesea llevar otro producto? (Si = 1, No = cualquier tecla)");
+			if(!leer.next().equals("1")) {
+				break;
+			}
+		}
+		
+		String columnas = "\nSu factura es\n\nItem\tID producto\tNombre producto\tCantidad\tValor unitario\tI.V.A.\tValor total";
+		System.out.println(columnas);
+		double valorTotalFactura = 0.0;
+		for(int i=0 ; i<FILAS_FACTURA ; i++) {
+			String valores = 
+					String.valueOf(matrizFactura[i][0]) + "\t" +
+					String.valueOf(matrizFactura[i][1]) + "\t\t" +
+					String.valueOf(matrizFactura[i][2]) + "\t\t" +
+					String.valueOf(matrizFactura[i][3]) + "\t\t" +
+					String.valueOf(matrizFactura[i][4]) + "\t\t" +
+					String.valueOf(matrizFactura[i][5]) + "\t" +
+					String.valueOf(matrizFactura[i][6]);
+			valorTotalFactura += (double)matrizFactura[i][6];
+			System.out.println(valores);
+			productosIngresados--;
+			if(productosIngresados == 0) {
+				break;
+			}			
+		}
+		System.out.println("Valor total a pagar: " + String.valueOf(valorTotalFactura));
+	}
+	
+	private static String obtenerNombreProducto(Object[][] matrizProductos, int idProducto) {
+		
+		String nombreProducto = "";
+		
+		for(int i=0 ; i<FILAS_PRODUCTO ; i++) {
+			if((int)matrizProductos[i][0] == idProducto) {
+				nombreProducto = (String)matrizProductos[i][1];
+				break;
+			}
+		}
+		
+		return nombreProducto;
+	} 
+	
+	private static double obtenerValorProducto(Object[][] matrizProductos, int idProducto) {
+		
+		double valorProducto = 0;
+		
+		for(int i=0 ; i<FILAS_PRODUCTO ; i++) {
+			if((int)matrizProductos[i][0] == idProducto) {
+				valorProducto = (double)matrizProductos[i][2];
+				break;
+			}
+		}
+		
+		return valorProducto;
+	} 
+	
+	private static double calcularIvaValorProducto(Object[][] matrizProductos, int idProducto) {
+		
+		double valorIva = 0.0;
+		
+		for(int i=0 ; i<FILAS_PRODUCTO ; i++) {
+			if((int)matrizProductos[i][0] == idProducto) {
+				if((boolean)matrizProductos[i][3] == true) {
+					valorIva = (double)matrizProductos[i][2] * 0.16;
+					break;
+				}
+			}
+		}
+		
+		return valorIva;
+	} 
+
+}
