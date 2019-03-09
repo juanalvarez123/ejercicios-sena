e041befa76fcb336ba6f78089f1c145fc1d488f2
Deissy Coral <Deissy Coral@DEI>
2019-03-09 16:41:58 -0500

    Se cerró una variable

--- Laboratorio/src/Laboratorio/Serie.java
+++ Laboratorio/src/Laboratorio/Serie.java
@@ -7,12 +7,13 @@ public class Serie {
     public static void main(String [] args){
         int x=0, c=0; 
         Scanner leer=new Scanner(System.in);
-        System.out.println("Este programa le permite ver los 20 primeros términos de la func�on f(x)=3x-C:");
+        System.out.println("Este programa le permite ver los 20 primeros términos de la func�on f(x)=3x-C:");
         System.out.println("Digite el valor de la constante: ");
         c=leer.nextInt();
         for (int i=0; i<20;i++){
             x=3*i-c;
             System.out.println("F("+i+")=3*"+i+"-"+c+"="+x);
         }
+        leer.close();
     }
 }
